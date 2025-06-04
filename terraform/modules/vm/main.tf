a1708ae100f805e9bda99b01ec644a5c99eb2ae0
Farah <abdifitah.a.f@gmail.com>
2025-06-05 00:58:08 +0100

    fixed terraform formatting

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  network_interface_ids = [ var.nic_id,]
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  network_interface_ids = [var.nic_id, ]
   admin_ssh_key {
     username   = "azureuser"
     public_key = file(var.ssh_key_bath) # Or pass via variable
   }
 
   os_disk {
-    name              = var.os_disk_name
-    caching           = "ReadWrite"
+    name                 = var.os_disk_name
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -1,31 +1,31 @@
 variable "vm_name" {
-  
+
 }
 
 variable "location" {
-  
+
 }
 
 variable "resource_group_name" {
-  
+
 }
 
 variable "nic_id" {
-  
+
 }
 
 variable "vm_size" {
-  
+
 }
 
 variable "admin_username" {
-  
+
 }
 
 variable "ssh_key_bath" {
-  
+
 }
 
 variable "os_disk_name" {
-  
+
 }
